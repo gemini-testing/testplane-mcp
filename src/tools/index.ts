a61d47d36044ccf6817b5c0526504ce4d70e6b0f
rocketraccoon <rocketraccoon@yandex-team.ru>
2025-07-31 18:15:16 +0700

    feat(mcp-tools): add hover element tool

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -2,6 +2,7 @@ import { ToolDefinition } from "../types.js";
 import { navigate } from "./navigate.js";
 import { closeBrowser } from "./close-browser.js";
 import { clickOnElement } from "./click-on-element.js";
+import { hoverElement } from "./hover-element.js";
 import { typeIntoElement } from "./type-into-element.js";
 import { takePageSnapshot } from "./take-page-snapshot.js";
 import { listTabs } from "./list-tabs.js";
@@ -14,6 +15,7 @@ export const tools = [
     navigate,
     closeBrowser,
     clickOnElement,
+    hoverElement,
     typeIntoElement,
     takePageSnapshot,
     listTabs,
